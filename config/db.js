f91bf960dd3949ca15984dec5b4491a3d23c46ec
christoper constancio <chris_constancio@hotmail.com>
2022-07-26 12:32:10 -0500

    fix db.js

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 
 dotenv.config();
-console.log(process.env.DB_USER)
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER,procees.env.DB_PASS,{
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,{
     host : process.env.DB_HOST, 
     port : '3306',
     dialect: 'mysql',
